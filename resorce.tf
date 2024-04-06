4ec6047efbab724ab31c3de3f6fd01c75a2e3155
Hitesh gupta <161241448+hiteshgupta1751@users.noreply.github.com>
2024-04-06 21:45:59 +0530

    resorce.tf

--- resorce.tf
+++ resorce.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web1" {
   ami                    = "ami-03035978b5aeb1274"
   instance_type          = "t3.micro"
+  count = 2
   availability_zone      = "eu-north-1a"
   vpc_security_group_ids = [aws_security_group.mysg.id]
   key_name               = "id_rsa"
@@ -10,22 +11,18 @@ resource "aws_instance" "web1" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.web1.public_ip} new_user=FIRST >> /root/terraform/inventory"
-  }
-}
-
-resource "aws_instance" "web2" {
-  ami                    = "ami-03035978b5aeb1274"
-  instance_type          = "t3.micro"
-  availability_zone      = "eu-north-1a"
-  vpc_security_group_ids = [aws_security_group.mysg.id]
-  key_name               = "id_rsa"
-
-  tags = {
-    Name = "web_Instance_2"
+    command = "echo ${aws_instance.web1.public_ip} "
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.web2.public_ip} new_user=SECOND >> /root/terraform/inventory"
+    command = "sleep 10"
   }
-
+  provisioner "local-exec" {
+    command = "ansible all -m ping"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook /root/terraform/web.yml"
+  }
 }
+
+
+
